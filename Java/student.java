02753bf55566ff37c5189d8b80f42babab56e1b5
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-01-22 12:05:16 +0530

    Create student.java

---
+++ Java/student.java
@@ -0,0 +1,20 @@
+public class student {
+        public static void main(String[] args) {
+          // Student data
+          String studentName = "Dharmu Kumar";
+          int studentID = 2420353;
+          int studentAge = 22;
+          float studentFee = 67000;
+          char studentGrade = 'A';
+          
+          // Print variables
+          System.out.println("Student name: " + studentName);
+          System.out.println("Student id: " + studentID);
+          System.out.println("Student age: " + studentAge);
+          System.out.println("Student fee: " + studentFee);
+          System.out.println("Student grade: " + studentGrade);
+        }
+      }
+      
+    
+
