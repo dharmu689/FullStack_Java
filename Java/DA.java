f0d62205333595f60141bf81bcbaddf203b39760
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-02-06 15:26:00 +0530

    Create DA.java

---
+++ Java/DA.java
@@ -0,0 +1,9 @@
+public class DA {
+    public static void main(String[] args) {
+        D Ba = new D();
+        System.out.println("Name: "+ Ba.Name);
+        System.out.println("Age: "+ Ba.age);
+        System.out.println("Sal: "+ Ba.sal);
+    }
+    
+}
