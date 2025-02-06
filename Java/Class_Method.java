8f3216b62848add54312175a740b47fc0110df1c
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-02-06 15:25:52 +0530

    Create Class_Method.java

---
+++ Java/Class_Method.java
@@ -0,0 +1,19 @@
+public class Class_Method {
+    // Create a fullThrottle() method
+  public void fullThrottle() {
+    System.out.println("The car is going as fast as it can!");
+  }
+
+  // Create a speed() method and add a parameter
+  public void speed(int maxSpeed) {
+    System.out.println("Max speed is: " + maxSpeed);
+  }
+
+  // Inside main, call the methods on the myCar object
+  public static void main(String[] args) {
+    Class_Method myCar = new Class_Method();     // Create a myCar object
+    myCar.fullThrottle();      // Call the fullThrottle() method
+    myCar.speed(200);          // Call the speed() method
+  }
+    
+}
